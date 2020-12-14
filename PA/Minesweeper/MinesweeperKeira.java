42e225fc7dbbd6ab17e1326787cd4da7cacd200b
Keira Schoolcraft <keira.schoolcraft@icloud.com>
2020-12-13 20:06:28 -0500

    templates(1)

---
+++ PA/Minesweeper/MinesweeperKeira.java
@@ -0,0 +1,97 @@
+// /************************************************************
+//   * CS112 Intro to Programming I
+//   * GS@IP Fall 20xx 
+//   *
+//   * Programming Assignment 6: Minesweeper
+//   * 
+//   * MinesweeperKeira.java represents the Minesweeper Game 
+//   * including the board and the play  * 
+//   * 
+//   * Author: Keira Schoolcraft
+//   * Description: For this PA, we have to make a program to make a minesweeper game
+//   * Honor Code: I will not use any other sources of code than my own
+//   * 
+//   * *********************************************************/
+
+//   import java.lang.Math;
+//   import java.util.Scanner;
+  
+//   public class MinesweeperKeira{// what is your first name?
+    
+//    /********* INSTANCEVARIABLES ***************/
+   
+//      // YOUR CODE GOES HERE
+     
+      
+//    /************** CONSTRUCTOR(s)  *************/
+  
+//      // YOUR CODE GOES HERE
+    
+//     public CellFirstname[][] gameBoard(int x, int y){
+      
+//       // initilaize the board  
+//         /***********************
+//          * YOUR CODE GOES HERE
+//          **********************/
+  
+//     }
+    
+//     public void countAdjacentMines(CellFirstname[][] game){
+      
+//       // count the adjacent mines of each cell and update each cell  
+//         /***********************
+//          * YOUR CODE GOES HERE
+//          **********************/    
+//     }
+    
+//     public void setAllContents(CellFirstname[][] game){
+     
+//       // count the adjacent mines of each cell and 
+//       // set the contents of a cell with the string you want to print to the screen (5pts) 
+//         /***********************
+//          * YOUR CODE GOES HERE
+//          **********************/    
+//     }
+    
+//     public void printBoard(CellFirstname[][] game, boolean mine){
+      
+//       // print the board and the revealed cells
+//       // this is to print the board when the game is in play  
+//         /***********************
+//          * YOUR CODE GOES HERE
+//          **********************/
+      
+//     }
+    
+//     public void play(CellFirstname[][] ){
+      
+      
+//       // ask the user for row and column number
+//       // if they enter an incorrect cell, ask them again
+      
+//       // if they stepped on a mine - the game is over 
+//       // print the board
+      
+//       // if they stepped on somewhere else
+//       // print the board
+     
+//         /***********************
+//          * YOUR CODE GOES HERE
+//          **********************/ 
+//     }
+    
+//     public boolean checkWinner(CellFirstname[][] game){
+      
+//       // check if the player has won
+//       // that means all the cells other than the mines are revealed
+//         /***********************
+//          * YOUR CODE GOES HERE
+//          **********************/
+//     }
+    
+//     // OPTIONAL
+//      public void revealNeighbors(int i, int j){
+    
+//     }
+    
+//   }
---
+++ PA/Minesweeper/TestKeira.java
@@ -0,0 +1,29 @@
+// package PA.Minesweeper;
+
+// /************************************************************
+//   * CS112 Intro to Programming I
+//   * GS@IP Fall 20xx
+//   * 
+//   * Programming Assignment 6: Minesweeper
+//   * 
+//   * TestYourname.java test the Minesweeper Game using corresponding 
+//   * Cell and Minesweeper classes 
+//   * 
+//   * YOU ARE NOT SUPPOSED TO MAKE ANY CHANGES TO THIS JAVA FILE
+//   * EXCEPT LINES 14 and 18, EXCEPT Minesweeper and Cell CLASS NAMES.
+//   * 
+//   * *********************************************************/
+  
+//   public class TestYourname{ // what is your first name?
+  
+//     public static void main(String[] args){
+      
+//       MinesweeperYourname m = new MinesweeperYourname(10, 10); // Minesweeper instance
+//       CellFirstname[][] ms = m.gameBoard(8, 15); // initialize the board
+//       m.setAllContents(ms); // set contents of each cell
+//       m.printBoard(ms, false); // print the initial board
+//       m.play(ms); // play the game
+      
+//     }
+    
+//   }
